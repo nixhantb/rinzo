8710b6735a3aca6a8ba6d421391f19e7322654e7
nixhantb <nishantbanjade07@gmail.com>
2025-04-21 11:53:17 +0100

    feat(domain): add Address value object as immutable record struct

---
+++ src/Rinzo.Domain/Entities/Common/Address.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Rinzo.Domain.Entities.Common
+{
+    /// <summary>
+    /// Address doesn't need its own identity (i.e., it's not an entity),
+    /// we can treat it as a Value Object.
+    /// </summary>
+    /// <param name="Street"></param>
+    /// <param name="City"></param>
+    /// <param name="State"></param>
+    /// <param name="PostalCode"></param>
+    /// <param name="Country"></param>
+    public record Address(
+        string Street,
+        string City,
+        string State,
+        string PostalCode,
+        string Country);
+}
