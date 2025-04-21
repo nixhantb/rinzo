e0f0168831263430f5d37cf93b1dbddfc950b067
nixhantb <nishantbanjade07@gmail.com>
2025-04-21 11:53:46 +0100

    feat(domain): add Salutation and Gender enums to support contact classification

---
+++ src/Rinzo.Domain/Enums/Gender.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rinzo.Domain.Enums
+{
+    public enum Gender
+    {
+        [Display(Name = "Unknown")]
+        Unknown = 0,
+
+        [Display(Name = "Female")]
+        Female = 1,
+
+        [Display(Name = "Male")]
+        Male = 2,
+
+        [Display(Name = "Transgender")]
+        Transgender = 3,
+
+        [Display(Name = "Non-Binary")]
+        NonBinary = 4,
+
+        [Display(Name = "Other")]
+        Other = 5
+    }
+}
---
+++ src/Rinzo.Domain/Enums/Salutation.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rinzo.Domain.Enums
+{
+    public enum Salutation
+    {
+        [Display(Name = "Mr.")]
+        Mr = 0,
+
+        [Display(Name = "Mrs.")]
+        Mrs = 1,
+
+        [Display(Name = "Miss")]
+        Miss = 2,
+
+        [Display(Name = "Ms.")]
+        Ms = 3,
+
+        [Display(Name = "Dr.")]
+        Dr = 4,
+
+        [Display(Name = "Prof.")]
+        Prof = 5,
+
+        [Display(Name = "Rev.")]
+        Rev = 6,
+
+        [Display(Name = "Other")]
+        Other = 7
+    }
+}
