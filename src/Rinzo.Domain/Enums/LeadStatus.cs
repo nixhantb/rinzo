1f4e244d8e5c9cae20d6b2bb33b131731973cfd7
nixhantb <nishantbanjade07@gmail.com>
2025-04-22 19:20:26 +0100

    add: lead status

---
+++ src/Rinzo.Domain/Enums/LeadStatus.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rinzo.Domain.Enums
+{
+    public enum LeadStatus
+    {
+        [Display(Name = "New")]
+        New = 1,
+
+        [Display(Name = "Contacted")]
+        Contacted = 2,
+
+        [Display(Name = "Qualified")]
+        Qualified = 3,
+
+        [Display(Name = "Unqualified")]
+        Unqualified = 4,
+
+        [Display(Name = "Converted")]
+        Converted = 5,
+
+        [Display(Name = "Disqualified")]
+        Disqualified = 6,
+
+        [Display(Name = "Follow Up")]
+        FollowUp = 7,
+
+        [Display(Name = "Lost")]
+        Lost = 8,
+
+        [Display(Name = "Nurturing")]
+        Nurturing = 9,
+
+        [Display(Name = "Recycled")]
+        Recycled = 10,
+
+        [Display(Name = "Archived")]
+        Archived = 11,
+
+        [Display(Name = "Closed")]
+        Closed = 12
+    }
+}
