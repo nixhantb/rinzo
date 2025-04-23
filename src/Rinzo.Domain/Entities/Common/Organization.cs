7a6c484e30eaf8a76598985482c35e446500bfa0
nixhantb <nishantbanjade07@gmail.com>
2025-04-23 20:53:04 +0100

    feat(domain): add Person and Organization with basic creation rules

---
+++ src/Rinzo.Domain/Entities/Common/Organization.cs
@@ -0,0 +1,48 @@
+﻿using Rinzo.Domain.Enums;
+using Rinzo.Domain.Primitives;
+
+namespace Rinzo.Domain.Entities.Common
+{
+    public class Organization : Entity
+    {
+        internal Organization(
+            string? organizationName,
+            EmployeesNumber? employeeNumbers,
+            decimal? annualRevenue,
+            string? website,
+            Guid id
+        ) : base(id)
+        {
+            OrganizationName = organizationName;
+            EmployeesNumbers = employeeNumbers;
+            Website = website;
+            AnnualRevenue = annualRevenue;
+        }
+
+        public string? OrganizationName { get; private set; }
+
+        public EmployeesNumber? EmployeesNumbers { get; private set; }
+
+        public decimal? AnnualRevenue { get; private set; }
+
+        public string? Website { get; private set; }
+
+       
+        public static Organization Create(
+            string? organizationName,
+            EmployeesNumber? employeeNumbers,
+            decimal? annualRevenue,
+            string? website
+        )
+        {
+
+            return new Organization(
+                organizationName,
+                employeeNumbers,
+                annualRevenue,
+                website,
+                Guid.NewGuid() 
+            );
+        }
+    }
+}
---
+++ src/Rinzo.Domain/Entities/Common/Person.cs
@@ -0,0 +1,39 @@
+﻿using Rinzo.Domain.Enums;
+using Rinzo.Domain.Primitives;
+
+namespace Rinzo.Domain.Entities.Common
+{
+    public class Person : Entity
+    {
+        /// <summary>
+        /// Private constructor used for controlled instantiation via factory methods or persistence.
+        /// </summary>
+        /// <param name="id">Unique identifier of the person</param>
+        /// <param name="salutationName">Salutation (e.g., Mr., Ms., Dr.)</param>
+        /// <param name="firstName">First name of the person</param>
+        /// <param name="lastName">Last name of the person</param>
+        /// <param name="email">Email address of the person</param>
+        /// <param name="phoneNumber">Phone number of the person</param>
+        internal Person(Guid id, Salutation? salutationType, string? firstName, string lastName, string? email, string? phoneNumber) : base(id)
+        {
+            SalutationType = salutationType;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Salutation? SalutationType { get; private set; }
+        public string? FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string? Email { get; private set; }
+        public string? PhoneNumber { get; private set; }
+
+        public static Person CreatePerson(Salutation? salutationType, string? firstName, string lastName, string? email, string? phoneNumber)
+        {
+            return new Person(Guid.NewGuid(), salutationType, firstName, lastName, email, phoneNumber);
+        }
+
+
+    }
+}
