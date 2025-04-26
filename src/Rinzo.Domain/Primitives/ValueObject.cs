8022d87fb0f68875c21743db83fe12ceca6fb05d
nixhantb <nishantbanjade07@gmail.com>
2025-04-26 12:09:30 +0100

    update(domain): ValueObjects intial starter code

---
+++ src/Rinzo.Domain/Primitives/ValueObject.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Rinzo.Domain.Primitives
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetAtomicValues();
+
+        private bool ValuesAreEqual(ValueObject other)
+        {
+            if(other is null)
+            {
+                return false;
+            }
+            return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+        }
+        public override bool Equals(object? obj)
+        {
+           if(obj is ValueObject other)
+            {
+                return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return GetAtomicValues()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return other is not null && ValuesAreEqual(other);
+        }
+    }
+}
