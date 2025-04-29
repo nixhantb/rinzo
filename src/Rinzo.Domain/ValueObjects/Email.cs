b9e568ed8d4b472e59112f1a1beadf99e5a1546b
nixhantb <nishantbanjade07@gmail.com>
2025-04-29 22:03:22 +0100

    add(domain): Email and Phone value objects

---
+++ src/Rinzo.Domain/ValueObjects/Email.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Rinzo.Domain.Primitives;
+using Rinzo.Domain.Shared;
+namespace Rinzo.Domain.ValueObjects
+{
+    public class Email : ValueObject
+    {
+        public string Value { get; }
+        public Email(string value)
+        {
+            Value = value;
+        }
+        public static Result<Email> Create(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return Result.Failure<Email>(new Error("Email.Empty", "Email is Empty"));
+            }
+            if (email.Length > 100)
+            {
+                return Result.Failure<Email>(new Error("Email.NameTooLong", "Email is too long"));
+            }
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                return Result.Failure<Email>(new Error("Email.InvalidFormat", "Email format is invalid"));
+            }
+            return new Email(email);
+        }
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+    }
+    {
+    }
+}
---
+++ src/Rinzo.Domain/ValueObjects/Phone.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Rinzo.Domain.Primitives;
+using Rinzo.Domain.Shared;
+namespace Rinzo.Domain.ValueObjects
+{
+    public class Phone : ValueObject
+    {
+        public string Value { get; }
+        public Phone(string value)
+        {
+            Value = value;
+        }
+        public static Result<Phone> Create(string phone)
+        {
+            if (string.IsNullOrWhiteSpace(phone))
+            {
+                return Result.Failure<Phone>(new Error("Phone.Empty", "Phone is Empty"));
+            }
+            if (phone.Length > 20)
+            {
+                return Result.Failure<Phone>(new Error("Phone.NameTooLong", "Phone is too long"));
+            }
+            return new Phone(phone);
+        }
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+    }
+    
+}
