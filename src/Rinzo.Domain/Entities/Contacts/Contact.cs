05b14d77f537572ff1cd31dad9909a8119080d52
nixhantb <nishantbanjade07@gmail.com>
2025-05-07 08:24:58 +0100

    domain(add):  properties of Lead and Contact with relationships and omitted Entity of Person and Organization in Lead / Contact and used Ids instead.

--- src/Rinzo.Domain/Entities/Contacts/Contact.cs
+++ src/Rinzo.Domain/Entities/Contacts/Contact.cs
@@ -4,49 +4,38 @@ using Rinzo.Domain.Enums;
 using Rinzo.Domain.Primitives;
 
 namespace Rinzo.Domain.Entities.Contacts
-    // lead->converttodeal
+    // If leads gets converted to deal, we automatically get lead as Contact 
 {
-    
     public class Contact : Entity
     {
         /// <summary>
         /// Private constructor used for controlled instantiation via factory methods or persistence.
         /// </summary>
         /// <param name="id">Unique identifier of the contact</param>
-        /// <param name="firstName">First name of the contact</param>
-        /// <param name="lastName">Last name of the contact</param>
-        /// <param name="email">Email address of the contact</param>
-        /// <param name="phoneNumber">Phone number of the contact</param>
+        ///  <param name="person">Person of the contact</param>
         /// <param name="genderType">Gender of the contact</param>
         /// <param name="mailingAddress">Mailing address (value object)</param>
         private Contact(
-            Guid id, 
-            string? firstName, 
-            string lastName, 
-            string email, 
-            string phoneNumber, 
+            Guid id,
+            Person person,
             string jobTitle,
-            string organization,
+            Organization organization,
             Gender genderType, 
            
             Address mailingAddress) : base(id)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-            PhoneNumber = phoneNumber;
-            JobTitle = jobTitle;
-            Organization = organization;
-            GenderType = genderType;
+            PersonId = person.Id;
            
+            JobTitle = jobTitle;
+            OrganizationId = organization.Id;
+            GenderType = genderType;
+            GenderType = genderType;
             MailingAddress = mailingAddress;
         }
-        public string? FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Email { get; private set; }   
-        public string PhoneNumber { get; private set; }
+       
         public string JobTitle { get; private set; }
-        public string Organization { get; private set; }
+        public Guid PersonId { get; private set; }
+        public Guid OrganizationId { get; private set; }
         public Gender GenderType { get; private set; }
         public Address MailingAddress { get; private set; }
         public DateTime CreatedAt { get; private set; }
--- src/Rinzo.Domain/Entities/Lead/Lead.cs
+++ src/Rinzo.Domain/Entities/Lead/Lead.cs
@@ -1,20 +1,24 @@
 ﻿using Rinzo.Domain.Entities.Common;
+using Rinzo.Domain.Entities.Contacts;
 using Rinzo.Domain.Enums;
 using Rinzo.Domain.Primitives;
-using Rinzo.Domain.Entities.Contacts;
+
+
 namespace Rinzo.Domain.Entities.Lead
 {
     public sealed class Lead : Entity
     {
         private readonly List<LeadActivity> _activities = new();
         public IReadOnlyCollection<LeadActivity> Activities => _activities.AsReadOnly();
+
         /// <summary>
         /// Private constructor used for controlled instantiation via factory methods or persistence.
         /// </summary>
         /// <param name="id">Unique identifier of the lead</param>
-        /// <param name="Person">Person information</param>
-        /// <param name="email">Email address of the lead</param>
-        /// <param name="phoneNumber">Phone number of the lead</param>
+        /// <param name="personId">ID of the person</param>
+        /// <param name="organizationId">ID of the organization</param>
+        /// <param name="leadOwnerId">ID of the lead owner (contact)</param>
+        /// <param name="status">Current status of the lead</param>
         private Lead(
             Guid id,
             Person person,
@@ -22,20 +26,18 @@ namespace Rinzo.Domain.Entities.Lead
             Contact leadOwner,
             LeadStatus status) : base(id)
         {
-            PersonDetails = person;
-            Organization = organization;
+            PersonId = person.Id;
+            OrganizationId = organization.Id;
             LeadOwnerId = leadOwner.Id;
             Status = status;
-
-
         }
-
-        public Person PersonDetails { get; set; }
-        public Organization Organization { get; set; }
+        public Guid PersonId { get; private set; }
+        public Guid OrganizationId { get; private set; }
         public Guid LeadOwnerId { get; private set; }
+
         public LeadStatus Status { get; private set; }
 
-       public void AddNote(Guid createdBy, string content)
+        public void AddNote(Guid createdBy, string content)
         {
             var note = new NoteActivity(Guid.NewGuid(), Id, createdBy, content);
             _activities.Add(note);
@@ -53,7 +55,5 @@ namespace Rinzo.Domain.Entities.Lead
 
             _activities.Add(taskActivity);
         }
-
-
     }
 }
