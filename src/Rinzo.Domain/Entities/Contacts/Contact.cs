40b1aa74ac0ad3527a328bc9840eae615f1322f2
nixhantb <nishantbanjade07@gmail.com>
2025-05-14 22:16:35 +0100

    update changes for the Lead and Contact

--- src/Rinzo.Domain/Entities/Contacts/Contact.cs
+++ src/Rinzo.Domain/Entities/Contacts/Contact.cs
@@ -12,33 +12,70 @@ namespace Rinzo.Domain.Entities.Contacts
         /// Private constructor used for controlled instantiation via factory methods or persistence.
         /// </summary>
         /// <param name="id">Unique identifier of the contact</param>
-        ///  <param name="person">Person of the contact</param>
+        /// <param name="person">Person of the contact</param>
+        /// <param name="jobTitle">Job title of the contact</param>
+        /// <param name="organization">Organization associated with the contact</param>
         /// <param name="genderType">Gender of the contact</param>
         /// <param name="mailingAddress">Mailing address (value object)</param>
-        private Contact(
+        internal Contact(
             Guid id,
             Person person,
             string jobTitle,
             Organization organization,
             Gender genderType, 
-           
             Address mailingAddress) : base(id)
         {
+            Person = person ?? throw new ArgumentNullException(nameof(person));
             PersonId = person.Id;
-           
             JobTitle = jobTitle;
+
+            Organization = organization ?? throw new ArgumentNullException(nameof(organization));
             OrganizationId = organization.Id;
             GenderType = genderType;
             GenderType = genderType;
             MailingAddress = mailingAddress;
+            CreatedOn = DateTime.UtcNow;
         }
        
-        public string JobTitle { get; private set; }
+
+       
+        public Person Person { get; private set; }
         public Guid PersonId { get; private set; }
+        public string JobTitle { get; private set; }
+        public Organization Organization { get; private set; }
         public Guid OrganizationId { get; private set; }
         public Gender GenderType { get; private set; }
         public Address MailingAddress { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-        public DateTime? UpdatedAt { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public DateTime? UpdatedOn { get; private set; }
+        
+
+        // Factory Method for creating a new Contact
+
+        public static Contact Create(
+            Person person,
+            string jobTitle,
+            Organization organization,
+            Gender genderType, 
+            Address mailingAddress
+
+            )
+        {
+
+            return new Contact(
+                    
+                    id: Guid.NewGuid(),
+                    person: person,
+                    jobTitle: jobTitle,
+                    organization: organization,
+                    genderType: genderType,
+                    mailingAddress: mailingAddress
+                );
+
+        }
+        
+
     }
+
+
 }
--- src/Rinzo.Domain/Entities/Lead/Lead.cs
+++ src/Rinzo.Domain/Entities/Lead/Lead.cs
@@ -26,17 +26,26 @@ namespace Rinzo.Domain.Entities.Lead
             Contact leadOwner,
             LeadStatus status) : base(id)
         {
+            Person = person ?? throw new ArgumentNullException(nameof(person));
+            Organization = organization ?? throw new ArgumentNullException(nameof(organization));
+            LeadOwner = leadOwner ?? throw new ArgumentNullException(nameof(leadOwner));
+
             PersonId = person.Id;
             OrganizationId = organization.Id;
             LeadOwnerId = leadOwner.Id;
             Status = status;
+            CreatedOn = DateTime.UtcNow;
         }
+        public Person Person { get; private set; }
+        public Organization Organization { get; private set; }
+        public Contact LeadOwner { get; private set; }
         public Guid PersonId { get; private set; }
         public Guid OrganizationId { get; private set; }
         public Guid LeadOwnerId { get; private set; }
 
         public LeadStatus Status { get; private set; }
-
+        public DateTime CreatedOn { get; private set; }
+        public DateTime? UpdatedOn { get; private set; }
         public void AddNote(Guid createdBy, string content)
         {
             var note = new NoteActivity(Guid.NewGuid(), Id, createdBy, content);
@@ -55,5 +64,6 @@ namespace Rinzo.Domain.Entities.Lead
 
             _activities.Add(taskActivity);
         }
+
     }
 }
