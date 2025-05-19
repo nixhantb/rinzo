f9b35f81a8d0bebcd3024556e1ed51003018437f
nixhantb <nishantbanjade07@gmail.com>
2025-05-19 22:16:55 +0100

    domain(add): create the command for lead and its handler (WIP)

---
+++ src/Rinzo.Application/Leads/Commands/CreateLeadCommand.cs
@@ -0,0 +1,13 @@
+﻿
+using MediatR;
+using Rinzo.Domain.Enums;
+namespace Rinzo.Application.Leads.Commands
+{
+    public sealed record CreateLeadCommand
+    (
+        Guid PersonId,
+        Guid OrganizationId,
+        Guid LeadOwnerId,
+        LeadStatus InitialStatus= LeadStatus.New
+    ):  IRequest<Guid>;
+}
---
+++ src/Rinzo.Application/Leads/Commands/CreateLeadCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using MediatR;
+using Rinzo.Domain.Repositories;
+
+namespace Rinzo.Application.Leads.Commands
+{
+    internal sealed class CreateLeadCommandHandler : IRequestHandler<CreateLeadCommand, Guid>
+    {
+        private readonly IContactRepository _contactRepository;
+        private readonly ILeadRepository _leadRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateLeadCommandHandler(
+            IContactRepository contactRepository,
+            ILeadRepository leadRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _contactRepository = contactRepository;
+            _leadRepository = leadRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public Task<Guid> Handle(CreateLeadCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
