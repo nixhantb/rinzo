678c781c7550a1aff9af7c86499616cdfc104be2
nixhantb <nishantbanjade07@gmail.com>
2025-05-23 19:31:01 +0100

    domain(add) : update IContactRepository

--- src/Rinzo.Domain/Repositories/IContactRepository.cs
+++ src/Rinzo.Domain/Repositories/IContactRepository.cs
@@ -5,5 +5,6 @@ namespace Rinzo.Domain.Repositories
     public interface IContactRepository
     {
         Task<Contact?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        void AddContact (Contact contact);  
     }
 }
