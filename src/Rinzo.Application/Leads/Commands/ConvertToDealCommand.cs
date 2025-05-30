58124bfbc8a226df8cc6b161888ad10e2fabe40f
nixhantb <nishantbanjade07@gmail.com>
2025-05-30 19:49:10 +0100

    application(add): add command for the ConvertToDealCommand

---
+++ src/Rinzo.Application/Leads/Commands/ConvertToDealCommand.cs
@@ -0,0 +1,16 @@
+﻿
+
+using MediatR;
+using Rinzo.Domain.Entities.Common;
+using Rinzo.Domain.Shared;
+
+namespace Rinzo.Application.Leads.Commands
+{
+    public sealed record ConvertToDealCommand
+    (
+        Guid LeadId,
+        string JobTitle,
+        Address MailingAddress
+
+    ) : IRequest<Result<Guid>>;
+}
