616a3decfadfa3c7e0bd648caae74d31a6293969
nixhantb <nishantbanjade07@gmail.com>
2025-06-01 11:51:22 +0100

    application(add): LeadNote command and the handler function for the lead note

---
+++ src/Rinzo.Application/Leads/Commands/AddLeadNote.cs
@@ -0,0 +1,12 @@
+﻿
+
+using MediatR;
+
+namespace Rinzo.Application.Leads.Commands
+{
+    public sealed record AddLeadCommand(
+        Guid LeadId,
+        Guid CreatedBy,
+        string Content
+        ): IRequest<bool>;
+}
---
+++ src/Rinzo.Application/Leads/Commands/AddLeadNoteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿
+
+using MediatR;
+using Rinzo.Domain.Repositories;
+
+namespace Rinzo.Application.Leads.Commands
+{
+    internal sealed class AddLeadNoteCommandHandler: IRequestHandler<AddLeadCommand, bool>
+    {
+        private readonly ILeadRepository _leadRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddLeadNoteCommandHandler(ILeadRepository leadRepository, IUnitOfWork unitOfWork)
+        {
+            _leadRepository = leadRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(AddLeadCommand request, CancellationToken cancellationToken)
+        {
+            var lead = await _leadRepository.GetByIdAsync(request.LeadId, cancellationToken);
+            if (lead is null)
+            {
+                return false; 
+            }
+            lead.AddNote(request.CreatedBy, request.Content);
+            _leadRepository.UpdateLead(lead);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return true; 
+        }
+
+    }
+}
