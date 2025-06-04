20b36f272fede72d47772485d3fcfd29e43c49af
nixhantb <nishantbanjade07@gmail.com>
2025-06-04 21:08:28 +0100

    add: structure for the aggregate root

---
+++ src/Rinzo.Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Rinzo.Domain.Primitives
+{
+    // it helps to create a trasactional boundaries
+    internal class AggregateRoot : Entity
+    {
+        protected AggregateRoot(Guid id) 
+            : base(id)
+        {
+        }
+    }
+}
